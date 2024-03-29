7864228d843ebfe620258bb152d2da44a4da64cb
Stas <lavrinovich990@mail.ru>
2024-03-29 18:22:54 +0500

    TimeTrack

---
+++ App2/App2/RegisterPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App2
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class RegisterPage : ContentPage
+	{
+		public RegisterPage ()
+		{
+			InitializeComponent();
+            logo.Source = ImageSource.FromResource("TimeTrack.logo.png");
+			nextButton.BackgroundColor = Color.Transparent;
+        }
+	}
+}
